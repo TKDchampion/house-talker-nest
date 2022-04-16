2dfb6c59d5cc8dae86f781dc7cd5eca455e8e1d1
TKDchampion <as1219123@yahoo.com.tw>
2022-04-16 15:09:02 +0800

    add api name

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,26 @@ import { ConfigModule } from '@nestjs/config';
 import { MailModule } from './mail/mail.module';
 import { ArticleModule } from './article/article.module';
 import { CommentModule } from './comment/comment.module';
+import { RouterModule, Routes } from 'nest-router';
+
+const routes: Routes = [
+  {
+    path: '/api/v1',
+    module: AuthModule,
+  },
+  {
+    path: '/api/v1',
+    module: ArticleModule,
+  },
+  {
+    path: '/api/v1',
+    module: CommentModule,
+  },
+];
 
 @Module({
   imports: [
+    RouterModule.forRoutes(routes),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
